69b341219ab7d76ddb7d24d1f3abbd775b0d37bc
Chase Hoffman <crhoffman@student.fullsail.edu>
2019-08-28 00:59:56 -0400

    Started carousel for home page

---
+++ js/application.js
@@ -0,0 +1,8 @@
+class Application{
+    constructor(){
+        this.carousel();
+    }
+    carousel(){
+        let images = ["maxresdefault.jpg", "frank-mckenna-127338-unsplash.jpg", "andre-robillard-298-unsplash.jpg"];
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,3 @@
+window.addEventListener('DOMContentLoaded', function () {
+    let myApp = Factory.getInstance();
+});
