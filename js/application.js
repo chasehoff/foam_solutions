3ae4dd2afe595463047c473c2afbc0cd27c7bf52
Chase Hoffman <crhoffman@student.fullsail.edu>
2019-09-04 14:42:51 -0400

    Fixed main.js and application.js errors

--- js/application.js
+++ js/application.js
@@ -5,4 +5,12 @@ class Application{
     carousel(){
         let images = ["maxresdefault.jpg", "frank-mckenna-127338-unsplash.jpg", "andre-robillard-298-unsplash.jpg"];
     }
+    static getInstance() {
+        if (!Application._instance) {
+            Application._instance = new Application();
+            return Application._instance;
+        } else {
+            throw 'Application already exists!';
+        }
+    }
 }
