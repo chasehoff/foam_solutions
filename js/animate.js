a3d571432a093ba9bac901adaef51fd119f3ca06
chasehoff <crhoffman@student.fullsail.edu>
2019-10-30 01:27:15 -0400

    Final push

--- js/animate.js
+++
@@ -1,3 +0,0 @@
-$(window).on(function() {
-    $("#header-grid").fadeIn();
-});
--- js/application.js
+++ js/application.js
@@ -1,35 +1,5 @@
 class Application{
     constructor(){
-        this.index = 1;
-        this.images = ["image_carousel_1.png", "Group\ 43.png", "image_carousel_2.png"];
-        this.headerBg = document.querySelector('#crh-index-header');
-        this.bubbles = document.querySelectorAll('.crh-carousel-circle');
-        document.querySelector('.next').addEventListener('click', this.next.bind(this));
-        document.querySelector('.prev').addEventListener('click', this.prev.bind(this));
-    }
-    next(){
-        this.bubbles[this.index].classList.remove('crh-carousel-active');
-        this.index++;
-        if(this.index == 3){
-            this.index = 0;
-        }
-        this.bubbles[this.index].classList.add('crh-carousel-active');
-        this.setImage(this.index);
-    }
-    prev(){
-        this.bubbles[this.index].classList.remove('crh-carousel-active');
-        this.index--;
-        if(this.index == -1){
-            this.index = 2
-        }
-        this.bubbles[this.index].classList.add('crh-carousel-active');
-        this.setImage(this.index);
-    }
-    setImage(index){
-        this.headerBg.style.background = "url(\'../images/" + this.images[index] + "\')";
-        this.headerBg.style.backgroundSize = "cover";
-        this.headerBg.style.backgroundPosition = "center";
-        this.headerBg.style.backgroundRepeat = "no-repeat";
     }
     static getInstance() {
         if (!Application._instance) {
