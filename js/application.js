8fe1d5fa8b8d9b8c67433e62fe2a11be13ae036b
chasehoff <crhoffman@student.fullsail.edu>
2019-10-30 21:55:40 -0400

    Fixed noty notification

--- js/application.js
+++ js/application.js
@@ -1,5 +1,13 @@
 class Application{
     constructor(){
+        new Noty({
+            type: 'error',
+            layout: 'centerRight',
+            text: 'This site is currently under construction!',
+            timeout: '4000',
+            progressBar: 'true',
+            theme: 'semanticui'
+        }).show();
     }
     static getInstance() {
         if (!Application._instance) {
